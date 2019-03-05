6abd7fae3a3e3146751157352d8ff45f3464ebd1
andrewmcintosh <abmcintosh7@gmail.com>
2019-03-05 15:43:20 -0500

    creating boilerplate for redux

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <p>Dashboard Component</p>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
---
+++ src/components/layout/Modal.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export class Modal extends Component {
+  render() {
+    return (
+      <div>
+        <p>Modal Component</p>
+      </div>
+    );
+  }
+}
+
+export default Modal;
